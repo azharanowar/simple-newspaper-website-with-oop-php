f5573681c063e548c65ed40cd601162eae5416e8
azharanowar <azharanowar@gmail.com>
2022-11-09 23:21:40 +0600

    All categories click to open link with id and home page linking for logo

--- pages/home.php
+++ pages/home.php
@@ -15,7 +15,7 @@
             <div class="container">
                 <nav class="navbar navbar-expand-lg pb-0">
                     <div class="container-fluid">
-                        <a class="navbar-brand fs-4 fw-normal text-muted text-uppercase" href="#"><span class="text-primary">News</span> Daily</a>
+                        <a class="navbar-brand fs-4 fw-normal text-muted text-uppercase" href="action.php?page=home"><span class="text-primary">News</span> Daily</a>
                         <div class="collapse navbar-collapse text-end d-none d-md-block" id="topNavbar">
                             <form class="col-md-5 my-2 my-md-0 mx-auto me-0" role="search">
                                 <div class="input-group">
@@ -33,19 +33,20 @@
             <div class="container">
                 <nav class="navbar navbar-expand-lg pt-0">
                     <div class="container-fluid">
-                        <a class="navbar-brand fs-4 fw-normal text-muted text-uppercase d-block d-md-none" href="#"><span class="text-primary">News</span> Daily</a>
+                        <a class="navbar-brand fs-4 fw-normal text-muted text-uppercase d-block d-md-none" href="action.php?page=home"><span class="text-primary">News</span> Daily</a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainMenu" aria-controls="mainMenu" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                         <div class="collapse navbar-collapse text-end py-3 py-md-0" id="mainMenu">
                             <ul class="navbar-nav mx-auto" id="mainMenuUl">
                                 <li class="nav-item">
-                                    <a class="nav-link active" aria-current="page" href="#" id="homePageNavLink">Home</a>
+                                    <a class="nav-link active" aria-current="page" href="action.php?page=home" id="homePageNavLink">Home</a>
                                 </li>
 
                                 <?php foreach ($categories as $category) { ?>
                                     <li class="nav-item">
-                                        <a class="nav-link" aria-current="page" href="#"><?php echo $category['name']; ?></a>
+                                        <a class="nav-link" aria-current="page" href="action.php?page=category&id=<?php echo $category['id']; ?>"><?php echo $category['name']; ?></a>
                                     </li>
                                 <?php }?>
+
                             </ul>
                         </div>
                     </div>
